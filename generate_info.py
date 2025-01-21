61bd1699f3c555402732c50006dcd8918102cac5
安久琲奈/LunaticGhoulPiano <38567204+LunaticGhoulPiano@users.noreply.github.com>
2025-01-21 14:19:05 +0800

    Finish worksheet 2

--- generate_info.py
+++ generate_info.py
@@ -212,8 +212,11 @@ def generate(info):
         },
     }
     """
+
+    ## set credit msgs
     credits_mapping = {}
     if info.majors['主修學程一']['對應xlsx名'] == '資工' and info.majors['主修學程二']['對應xlsx名'] == '資工':
+        ## set msgs
         for key, value in info.credit_details['資工']['最低應修學分數']['雙資工']['必修+核心'].items():
             total_credit = value if info.majors['主修學程一']['學程名稱'] in key and info.majors['主修學程二']['學程名稱'] in key else '0'
         credits_mapping['主修學程一'] = {
@@ -264,14 +267,119 @@ def generate(info):
         '核心': { '基本要求': '', '額外要求': '' },
         '選修': { '基本要求': '', '額外要求': '' } }
     
-    # set headers
-    basic_header = [f"主修學程一：{info.majors['主修學程一']['學程名稱']}", '',  '', f"主修學程二：{info.majors['主修學程二']['學程名稱']}", '', '', f"副修學程一：{info.majors['副修學程']['學程名稱']}", '', '']
+    ## fetch majors info
+    majors_info = {}
+    for major in ['主修學程一', '主修學程二', '副修學程']:
+        majors_info[major] = {}
+        for single_type in ['必修', '核心', '選修']:
+            majors_info[major][single_type] = []
+            if info.majors[major]['對應xlsx名'] != '資工':
+                for course, course_dict in info.credit_details[info.majors[major]['對應xlsx名']][info.majors[major]['學程名稱']][single_type]['課程'].items():
+                    majors_info[major][single_type].append([course, course_dict['學分數'], course_dict['審查備註']])
+            else:
+                if single_type != '選修':
+                    for course, course_dict in info.credit_details['資工'][info.majors[major]['學程名稱']][single_type]['課程'].items():
+                        majors_info[major][single_type].append([course, course_dict['學分數'], course_dict['審查備註']])
+                else:
+                    tempstr = ''
+                    for four_type_name, four_type_code in info.credit_details['資工'][info.majors[major]['學程名稱']]['選修']['認列四大類'].items():
+                        tempstr = ' / '.join([tempstr, f'{four_type_name} ({four_type_code})']) if tempstr != '' else f'{four_type_name} ({four_type_code})'
+                    majors_info[major][single_type].append([f'認列：{tempstr}', '', ''])
+    
+    ## set max length of body type
+    column_len_mapping = {}
+    for cur_type in ['必修', '核心', '選修']:
+        column_len_mapping[cur_type] = 0
+        for cur_major in ['主修學程一', '主修學程二', '副修學程']:
+            if len(majors_info[cur_major][cur_type]) > column_len_mapping[cur_type]:
+                column_len_mapping[cur_type] = len(majors_info[cur_major][cur_type])
+    
+    ## set body
+    body = []
+    basic_header = [f"主修學程一：{info.majors['主修學程一']['學程名稱']}", '',  '', f"主修學程二：{info.majors['主修學程二']['學程名稱']}", '', '', f"副修學程：{info.majors['副修學程']['學程名稱']}", '', '']
+    body.append(basic_header)
     must_header = ['必修課程', credits_mapping['主修學程一']['必修']['基本要求'], credits_mapping['主修學程一']['必修']['額外要求'], '必修課程', credits_mapping['主修學程二']['必修']['基本要求'], credits_mapping['主修學程二']['必修']['額外要求'], '必修課程', credits_mapping['副修學程']['必修']['基本要求'], credits_mapping['副修學程']['必修']['額外要求']]
     core_header = ['核心課程', credits_mapping['主修學程一']['核心']['基本要求'], credits_mapping['主修學程一']['核心']['額外要求'], '核心課程', credits_mapping['主修學程二']['核心']['基本要求'], credits_mapping['主修學程二']['核心']['額外要求'], '核心課程', credits_mapping['副修學程']['核心']['基本要求'], credits_mapping['副修學程']['核心']['額外要求']]
-    elect_header = ['核心課程', credits_mapping['主修學程一']['選修']['基本要求'], credits_mapping['主修學程一']['選修']['額外要求'], '選修課程', credits_mapping['主修學程二']['選修']['基本要求'], credits_mapping['主修學程二']['選修']['額外要求'], '選修課程', credits_mapping['副修學程']['選修']['基本要求'], credits_mapping['副修學程']['選修']['額外要求']]
+    elect_header = ['選修課程', credits_mapping['主修學程一']['選修']['基本要求'], credits_mapping['主修學程一']['選修']['額外要求'], '選修課程', credits_mapping['主修學程二']['選修']['基本要求'], credits_mapping['主修學程二']['選修']['額外要求'], '選修課程', credits_mapping['副修學程']['選修']['基本要求'], credits_mapping['副修學程']['選修']['額外要求']]
     basic_sub_header = ['課程名稱', '學分數', '審查備註', '課程名稱', '學分數', '審查備註', '課程名稱', '學分數', '審查備註']
-    
+    for cur_type, header in zip(['必修', '核心', '選修'], [must_header, core_header, elect_header]):
+        body.append(header)
+        body.append(basic_sub_header)
+        for idx in range(column_len_mapping[cur_type]):
+            row_data = []
+            for cur_major in ['主修學程一', '主修學程二', '副修學程']:
+                if idx < len(majors_info[cur_major][cur_type]):
+                    row_data += majors_info[cur_major][cur_type][idx]
+                else:
+                    row_data += [''] * 3
+            body.append(row_data)
 
+    ## fetch cs elect - four types
+    all_four_type = {}
+    cs_elect = []
+    for major in ['主修學程一', '主修學程二', '副修學程']:
+        if info.majors[major]['對應xlsx名'] == '資工':
+            for four_type_name, four_type_code in info.credit_details['資工'][info.majors[major]['學程名稱']]['選修']['認列四大類'].items():
+                temp = []
+                temp.append(f'{four_type_name} {four_type_code}')
+                temp += info.credit_details['資工']['四大類'][four_type_name]['課程']
+                cs_elect.append(temp)
+    
+    ## align cs elect
+    max_cs_column_len = len(max(cs_elect, key = len))
+    for idx in range(max_cs_column_len):
+        for sub_list in cs_elect:
+            if idx < len(sub_list):
+                sub_list += [''] * (max_cs_column_len - len(sub_list))
+    
+    ## set body cell width
+    max_cell_width = [0 for _ in range(9)]
+    for row in body:
+        for i, cell in enumerate(row):
+            cell_length = sum(get_char_width(c) for c in str(cell))
+            if cell_length > max_cell_width[i]:
+                max_cell_width[i] = cell_length
+    for i in range(9):
+        ws.column_dimensions[chr(65 + i)].width = max_cell_width[i] + 10 # offset
+    
+    ## draw body
+    for row_index, row_data in enumerate(body, start = 1):
+        for column_index, cell_data in enumerate(row_data, start = 1):
+            cell = ws.cell(row = row_index, column = column_index, value = cell_data)
+        if row_index == 1:
+            for column in range(1, 10):
+                ws.cell(row = row_index, column = column).fill = PatternFill(start_color = '4BACC6', end_color = '4BACC6', fill_type = 'solid')
+                ws.cell(row = row_index, column = column).font = Font(bold = True)
+        elif '必修課程' in row_data[0] or '核心課程' in row_data[0] or '選修課程' in row_data[0]:
+            for column in range(1, 10):
+                ws.cell(row = row_index, column = column).fill = PatternFill(start_color = '92CDDC', end_color = '92CDDC', fill_type = 'solid')
+                ws.cell(row = row_index, column = column).font = Font(bold = True)
+        elif '課程名稱' in row_data[0]:
+            for column in range(1, 10):
+                ws.cell(row = row_index, column = column).fill = PatternFill(start_color = 'B7DEE8', end_color = 'B7DEE8', fill_type = 'solid')
+                ws.cell(row = row_index, column = column).font = Font(bold = True)
+
+    ## if have cs elect, then add to body
+    if cs_elect != []:
+        column_offset_idx = 9 + 1 # 1 for empty column to split cs four type and majors
+
+        ## set cs elect cell width
+        max_cell_width = [0 for _ in range(len(cs_elect))]
+        for column_index, column in enumerate(cs_elect):
+            for row_index, cell in enumerate(column):
+                cell_length = sum(get_char_width(c) for c in str(cell))
+                if cell_length > max_cell_width[column_index]:
+                    max_cell_width[column_index] = cell_length
+        for i in range(len(cs_elect)):
+            ws.column_dimensions[chr(65 + column_offset_idx + i)].width = max_cell_width[i] + 10 # offset
+        
+        ## draw cs elect
+        for column_index, column_data in enumerate(cs_elect, start = 1):
+            for row_index, cell_data in enumerate(column_data, start = 1):
+                cell = ws.cell(row = row_index, column = column_offset_idx + column_index, value = cell_data)
+                if row_index == 1:
+                    ws.cell(row = row_index, column = column_offset_idx + column_index).fill = PatternFill(start_color = 'B7DEE8', end_color = 'B7DEE8', fill_type = 'solid')
+                    ws.cell(row = row_index, column = column_offset_idx + column_index).font = Font(bold = True)
     # save workbook
     wb.save(excel_file_path)
 
